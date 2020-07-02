faa420725916e97856115f1cedc3b75a8b16a40a
@theship <juburdekin@gmail.com, julee@abevy.com>
2020-07-02 13:50:21 -0700

    added updated config file

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gres2Air',
+    description: 'Google Results to Airtable',
+    author: '@julee',
+    siteUrl: 'https://google.com',
+    image: 'https://source.unsplash.com/random/400x200',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Example',
+        short_name: 'example',
+        start_url: '/',
+        background_color: '#000000',
+        theme_color: '#000000',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        includePaths: ['./src/styles/'],
+      },
+    },
+    'gatsby-plugin-robots-txt',
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-source-airtable',
+      options: {
+        apiKey: 'YOUR_AIRTABLE_KEY',
+        tables: [
+          {
+            baseId: 'YOUR_AIRTABLE_BASE_ID',
+            tableName: 'Orgs',
+          },
+        ],
+      },
+    },
+  ],
+};
